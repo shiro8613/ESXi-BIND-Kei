5faf7cccd231fc7e8bf444fad890a62fcced2f76
toms0910 <psp1070@gmail.com>
2022-12-25 03:19:16 +0900

    debianに変更あーんどentrypoint.shをへんこうだにゃー－－ん

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ MODIFIED_PORT=`echo $(echo ${BIND_PORT} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 MODIFIED_IP=`echo $(echo ${ESXI_IP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 MODIFIED_DOMAIN=`echo $(echo ${DOMAIN_NAME} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
-cat << EOF > /data/config.yml
+cd /data
+
+cat << EOF > /config.yml
 port: $MODIFIED_PORT
 esxi_addr: $MODIFIED_IP
 domain_name: $MODIFIED_DOMAIN
 EOF
 
-./data/dns
+./dns
